9151353bd0a9a6251581d89b04bf51081a75a43c
Clementine Digout <clementine.digout@yahoo.fr>
2024-09-09 14:40:52 -0400

    Better responsive

--- src/assets/js/filters.js
+++ src/assets/js/filters.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const filtersDiv = document.querySelector('.filters');
     const projets = document.querySelectorAll('.div-projet');
     const filters = document.querySelectorAll('.checkbox-filter');
-    
 
     filterIcon.addEventListener('click', () => {
         filtersDiv.classList.toggle('hidden');
@@ -12,32 +11,34 @@ document.addEventListener('DOMContentLoaded', () => {
     filters.forEach(filter => {
         filter.addEventListener('change', () => {
             let activeFilters = [];
-            
-            // Parcourir tous les filtres pour trouver ceux qui sont cochés
+
+            // Trouver tous les filtres cochés
             filters.forEach(filter => {
                 if (filter.checked) {
                     activeFilters.push(filter.getAttribute('data-filter'));
                 }
             });
 
-            // Parcourir tous les projets pour afficher ou masquer selon les filtres actifs
+            // Afficher ou masquer les projets en fonction des filtres actifs
             projets.forEach(projet => {
                 const projetCategories = projet.getAttribute('data-filter').split(' ');
-                let isMatch = true;
 
-                // Vérifier si le projet correspond à tous les filtres actifs
-                activeFilters.forEach(filter => {
-                    if (!projetCategories.includes(filter)) {
-                        isMatch = false;
-                    }
-                });
+                // Si aucun filtre n'est actif, afficher tous les projets
+                if (activeFilters.length === 0) {
+                    projet.style.display = '';
+                    return;
+                }
 
-                // Afficher ou masquer le projet
-                if (isMatch || activeFilters.length === 0) {
+                // Si le projet correspond à au moins un filtre actif, l'afficher
+                //some() permet de vérifier si au moins un élément d'un tableau répond à la condition donnée
+                const isMatch = activeFilters.some(filter => projetCategories.includes(filter));
+
+                if (isMatch) {
                     projet.style.display = '';
                 } else {
                     projet.style.display = 'none';
                 }
+                
             });
         });
     });
